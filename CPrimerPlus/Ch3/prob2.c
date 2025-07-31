43b955145fd359eb901ca0a220b74f0717676245
S <ssoehnlen@protonmail.com>
2025-07-31 12:54:55 +0200

    final file ch3, and files for ch4

--- CPrimerPlus/Ch3/prob2.c
+++ CPrimerPlus/Ch3/prob2.c
@@ -1,15 +1,14 @@
-/* accepts ascii value and prints corresponding ascii character */
+/* accepts ASCII code value and prints corresponding ascii character */
 
 #include<stdio.h>
 int main(void)
 {
     int  ch;    
     
-    printf("Enter an ASCII code value: \n");
-    scanf("%c", &ch);
-    printf("The ASCII character for %c is %d.\n",ch, ch);
-
-    printf("The  dude for %d is %c.\n",ch, ch);
+    printf("Enter a value to convert to ASCII code: \n");
+    scanf("%d", &ch);
+    
+    printf("The ASCII code for %d is %c.\n",ch, ch);
 
     return 0;
 }
