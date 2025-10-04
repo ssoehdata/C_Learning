12bb13bdcde7b1af8ec5c51c6af727281d244582
S <ssoehnlen@protonmail.com>
2025-10-04 18:35:03 +0200

    adding ch6 examples src

---
+++ CPrimerPlus/Ch6/6_11.c
@@ -0,0 +1,16 @@
+// sweetie1.c --a counting loop 
+/* inline version of 6_10 contains all 3 expressions prev
+   put into separate lines / block */
+#include<stdio.h>
+int main(void)
+{
+    const int NUMBER = 22;
+    int count = 1;                      
+
+    for (count =1; count <= NUMBER; count++)             
+    
+        printf("Be my Valentine!\n");                        
+    
+
+    return 0;
+}
