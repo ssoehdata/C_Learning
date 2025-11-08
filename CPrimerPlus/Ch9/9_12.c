b6c4c3761e52557d36668324525924492026ab59
S <ssoehnlen@protonmail.com>
2025-11-08 14:34:17 +0100

    adding src

---
+++ CPrimerPlus/Ch9/9_12.c
@@ -0,0 +1,27 @@
+/* loccheck.c  --checks  to see where variables are storeed  */
+#include<stdio.h> 
+void mikado(int);                      /* declare function */ 
+int main(void)
+{
+    int pooh = 2, bah = 5;          /* local to main() */
+
+    printf("In main(), pooh = %d and &pooh = %p\n", 
+        pooh, &pooh);
+
+    printf("In main(), bah = %d and %bah = %p\n",
+        bah, &bah);
+        mikado(pooh);
+
+        return 0;
+}
+void mikado(int bah)                    /* define function  */
+{
+    int pooh = 0;                      /* local to mikado( ) */
+
+    printf("In mikado(), pooh = %d and &pooh = %p\n",
+        pooh, &pooh);
+    printf("In mikado(), bah = %d and &bah - %p\n",
+        bah, &bah);
+
+}
+
