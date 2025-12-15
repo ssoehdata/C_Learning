18931f3424badb19e66b18febd0e50a7668bd00a
S <ssoehnlen@protonmail.com>
2025-12-15 17:02:52 +0100

    adding ch16 files

---
+++ CPrimerPlus/Ch16/16_1.c
@@ -0,0 +1,22 @@
+/* preparec. --simple preprocessor examples */
+#include<stdio.h> 
+#define TWO 2           /* you can  use comments if you like */
+#define OW "Consistency is the last refuge of unimagina\
+tive. - Oscar Wilde" /* a backslash continues a definition */
+                 /* to the next line                */
+#define FOUR TWO*TWO 
+#define PX printf("X is %d.\n", x)
+#define FMT "X is  %d.\n"
+
+int main(void)
+{
+    int x = TWO;
+
+    PX;
+    x = FOUR;
+    printf(FMT, x);
+    printf("%s\n", OW);
+    printf("TWO: OW\n");
+
+    return 0;
+}
