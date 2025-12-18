3dc3addbef9b82a413c8510950fb7f07b11bf45f
S <ssoehnlen@protonmail.com>
2025-12-18 15:27:28 +0100

    adding src ch12

---
+++ CPrimerPlus/Ch12/12_11.c
@@ -0,0 +1,37 @@
+/* diceroll.c -- dice role simulation */
+
+#include "diceroll.h"
+#include<stdio.h> 
+#include<stdlib.h>
+
+int roll_count = 0;     /* external linkage */
+
+static int rollem(int sides) /* private to the file */
+{
+    int roll;
+    roll = rand( ) % sides + 1;
+    ++roll_count;       /* count function calls */
+
+    return roll;
+}
+
+int roll_n_dice(int dice, int sides)
+{
+    int d;
+    int total = 0;
+    if (sides < 2) 
+    {
+        printf("Need at least 2 sides.\n");
+        return -2;
+    }
+    if (dice < 1) 
+    {
+        printf("Need at least 1 die.\n");
+        return -1;
+    }
+
+    for  (d = 0; d < dice; d++)
+        total += rollem(sides);
+
+    return total;
+}
---
+++ CPrimerPlus/Ch12/diceroll.h
@@ -0,0 +1,7 @@
+//diceroll.h 
+extern int roll_count; 
+
+int roll_n_dice(int dice, int sides);
+
+// extern int roll_count;      // from header file 
+// int roll_count = 0;      // from source code file 
