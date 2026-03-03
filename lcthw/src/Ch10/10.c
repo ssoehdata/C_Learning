1b1d11f7cad6346db7609801512dfb2d545c4bf5
s <ssoehnlen@protonmail.com>
2026-03-03 12:48:41 +0100

    correcting error in ch10 src

--- lcthw/src/Ch10/10.c
+++ lcthw/src/Ch10/10.c
@@ -1,25 +1,66 @@
 #include<stdio.h> 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
+	if (argc !=2) {
+	printf("ERROR: You need one argument.\n");
+	return 1;
+	}
 	int i = 0;
+	for (i = 0; argv[1][i] != '\0'; i++) {
+	 	char letter = argv[1][i];
 
-	// go through each string in argv
-	// why am I skipping argv[0]?
-	for (i = 1; i < argc; i++) {
-		printf("arg %d: %s\n", i,argv[i]);
+		switch (letter) {
+			case 'a':
+			case 'A':
+				printf("%d: 'A'\n", i);
+				break;
+
+            case 'e':
+			case 'E':
+				printf("%d: 'E'\n", i);
+				break;
+
+            case 'i':
+			case 'I':
+				printf("%d: 'I'\n", i);
+				break;
+
+            case 'o':
+			case 'O':
+				printf("%d: 'O'\n", i);
+				break;
+
+            case 'u':
+			case 'U':
+				printf("%d: 'U'\n", i);
+				break;
+	    
+            case 'y':
+			case 'Y':
+                if (i > 2) {
+                    printf("%d: 'Y'\n", i);
+                }
+                break;
+           default: 
+                    printf("%d: %c is not a vowel\n", i, letter);
+            }		
+				
 	}
-
-	// let's make our own array of strings
-	char *states[] = {
-		"Montana", "Oregon",
-		"Washington", "Texas"
-	};
-	
-	int num_states = 4;
-
-	for (i = 0; i < num_states; i++) {
-		printf("state %d: %s\n", i, states[i]);
-	}
-
-	 return 0;
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
